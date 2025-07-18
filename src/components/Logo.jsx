7c817a8e6f9722cfe2f1218598425ed63927d61d
Eesh Midha <72607015+eeshm@users.noreply.github.com>
2025-07-18 15:33:46 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ import Logo1 from '../../public/blogitup.png'
 function Logo({width}) {
   return (
     <div >
-      <img src={Logo1} alt="Logo" style={{width:150}} className='-mt-12' />;
+      <img src={Logo1} alt="Logo" style={{width:150}} className='-mt-12' />
     </div>
   )
 }
